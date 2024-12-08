4f8a97ef037a3743d9beb16ede3de788864df280
Bagas Rosyidi <bagasrosyidi@gmail.com>
2024-12-08 07:36:29 +0700

    Update server.js
    update port,host

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,8 @@ import { InputError } from '../exceptions/InputError.js';
 
 (async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
